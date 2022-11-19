bcb9169383fc44ad6cc51ac1bea80a8c2e306210
Adaura <96139981+Adaura@users.noreply.github.com>
2022-11-19 16:44:46 +0100

    teste

---
+++ projet45/cmake-build-debug/CMakeFiles/projet45.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet45.dir/fonction.c.obj"
+  "CMakeFiles/projet45.dir/fonction.c.obj.d"
+  "CMakeFiles/projet45.dir/main.c.obj"
+  "CMakeFiles/projet45.dir/main.c.obj.d"
+  "libprojet45.dll.a"
+  "projet45.exe"
+  "projet45.exe.manifest"
+  "projet45.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet45.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- projet45/cmake-build-debug/cmake_install.cmake
+++ projet45/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/David/Documents/GitHub/Theorie-des-grapphes/projet45
+# Install script for directory: C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/projet45
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/CodeBlocks/MinGW/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/David/Documents/GitHub/Theorie-des-grapphes/projet45/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/projet45/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
