36b19ae961aae576ba500bdb93621700ee96bf8b
Adaura <96139981+Adaura@users.noreply.github.com>
2022-11-23 00:29:47 +0100

    nice

--- ece-city/cmake-build-debug/CMakeFiles/VerifyGlobs.cmake
+++ ece-city/cmake-build-debug/CMakeFiles/VerifyGlobs.cmake
@@ -3,20 +3,20 @@
 cmake_policy(SET CMP0009 NEW)
 
 # project_SRC at CMakeLists.txt:5 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/ece-city/*.c")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.4/ece-city/*.c")
 set(OLD_GLOB
-  "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/ece-city/main.c"
+  "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.4/ece-city/main.c"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/ece-city/cmake-build-debug/CMakeFiles/cmake.verify_globs")
+  file(TOUCH_NOCREATE "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.4/ece-city/cmake-build-debug/CMakeFiles/cmake.verify_globs")
 endif()
 
 # project_SRC at CMakeLists.txt:5 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/ece-city/*.h")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.4/ece-city/*.h")
 set(OLD_GLOB
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.2/Theorie-des-grapphes/ece-city/cmake-build-debug/CMakeFiles/cmake.verify_globs")
+  file(TOUCH_NOCREATE "C:/Users/adame/Documents/Travail/ECE Ing 2/Semestre 1/Theorie des graphes/Projet/V.4/ece-city/cmake-build-debug/CMakeFiles/cmake.verify_globs")
 endif()
